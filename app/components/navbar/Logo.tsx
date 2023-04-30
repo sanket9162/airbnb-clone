b10ed9d7cd410440f0b4825319466371a847cd84
Sanket Gondhali <sanketgondhali55@gmail.com>
2023-04-30 10:09:39 +0530

    first commit

---
+++ app/components/navbar/Logo.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import Image from "next/image";
+
+
+const Logo = () => {
+  
+
+  return ( 
+    <Image
+      
+      className="hidden md:block cursor-pointer" 
+      src="/Images/logo.png" 
+      height="100" 
+      width="100" 
+      alt="Logo" 
+    />
+   );
+}
+ 
+export default Logo;
